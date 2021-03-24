4a31ea80ed9bf01eb6d21905efd67f9518cf7de3
weilly0912 <fre5x520@gmail.com>
2021-03-24 19:25:31 +0800

    update proj3 folder

---
+++ Algorithms and Applications Tutorials/proj3_6320/proj3_code/student_feature_matching.py
@@ -0,0 +1,86 @@
+import numpy as np
+
+
+def compute_feature_distances(features1, features2):
+    """
+    This function computes a list of distances from every feature in one array
+    to every feature in another.
+    Args:
+    - features1: A numpy array of shape (n,feat_dim) representing one set of
+      features, where feat_dim denotes the feature dimensionality
+    - features2: A numpy array of shape (m,feat_dim) representing a second set
+      features (m not necessarily equal to n)
+
+    Returns:
+    - dists: A numpy array of shape (n,m) which holds the distances from each
+      feature in features1 to each feature in features2
+    """
+
+    ###########################################################################
+    # TODO: YOUR CODE HERE                                                    #
+    ###########################################################################
+
+    dists = np.empty((features1.shape[0], features2.shape[0]))
+    for i in range(features1.shape[0]):
+        for j in range(features2.shape[0]):
+            dists[i, j] = np.linalg.norm(features1[i, :] - features2[j, :])
+
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+
+    return dists
+
+
+def match_features(features1, features2, x1, y1, x2, y2):
+    """
+    This function does not need to be symmetric (e.g. it can produce
+    different numbers of matches depending on the order of the arguments).
+
+    To start with, simply implement the "ratio test", equation 4.18 in
+    section 4.1.3 of Szeliski. There are a lot of repetitive features in
+    these images, and all of their descriptors will look similar. The
+    ratio test helps us resolve this issue (also see Figure 11 of David
+    Lowe's IJCV paper).
+
+    You should call `compute_feature_distances()` in this function, and then
+    process the output.
+
+    Args:
+    - features1: A numpy array of shape (n,feat_dim) representing one set of
+      features, where feat_dim denotes the feature dimensionality
+    - features2: A numpy array of shape (m,feat_dim) representing a second
+      set of features (m not necessarily equal to n)
+    - x1: A numpy array of shape (n,) containing the x-locations of features1
+    - y1: A numpy array of shape (n,) containing the y-locations of features1
+    - x2: A numpy array of shape (m,) containing the x-locations of features2
+    - y2: A numpy array of shape (m,) containing the y-locations of features2
+
+    Returns:
+    - matches: A numpy array of shape (k,2), where k is the number of matches.
+      The first column is an index in features1, and the second column is an
+      index in features2
+    - confidences: A numpy array of shape (k,) with the real valued confidence
+      for every match
+
+    'matches' and 'confidences' can be empty e.g. (0x2) and (0x1)
+    """
+
+    ###########################################################################
+    # TODO: YOUR CODE HERE                                                    #
+    ###########################################################################
+
+    dist = compute_feature_distances(features1, features2)
+    dist_sort = np.sort(dist, axis=1)
+    ratioTest = dist_sort[:, 0] / dist_sort[:, 1]
+    tune = 0.7
+    confidences = ratioTest[ratioTest < tune]
+    index = np.argsort(dist, axis=1)
+    tmp = np.transpose(np.nonzero(ratioTest < tune)).flatten()
+    matches = np.stack([tmp, index[ratioTest < tune, 0]], axis=1)
+
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+
+    return matches, confidences
